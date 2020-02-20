037b616f456e2cf6e1fadfcedefc53cb6e804be9
Lance Lao <lancelao99@gmail.com>
2020-02-20 23:48:25 +0800

    pages for successful and failure in creating account

---
+++ assets/php/account_success.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Cubers</title>
+  
+  
+  <link rel='stylesheet prefetch' href="https://fonts.googleapis.com/css?family=Raleway&display=swap">
+      <link rel="stylesheet" href="css/style.css">
+  
+</head>
+<body>
+<div class="text-box">
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <div class="header">Account Successfully created</div>
+      </div>
+    </div>
+    </body>
+</html>
